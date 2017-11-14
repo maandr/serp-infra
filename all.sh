71ec3694317c64a44ab44b0210f449ec6c00c35f
Mats Andresen <m.andresen@epages.com>
2017-11-14 19:33:34 +0100

    inital commit

---
+++ all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./infrastructure.sh $@
+
+sleep 3
+
+./serp.sh $@
---
+++ env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DOCKER_USER=maandr
+
+export IMAGE_NAME=serp-api
+export IMAGE_VERSION=1.0.0
+
+export APPLICATION_PORT=3000
+export APPLICATION_NAME="serp-api"
+
+export HOST_PORT=80
